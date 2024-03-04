f02b684b69ddf7a2683cb6bd8ec523042847ddaf
Praveen Saini <praveensaini@admins-MacBook-Pro.local>
2024-03-04 10:54:44 +0530

    added logger singleton assignment

--- src/lld1/designPattern/singletonDesignPattern/assignments/fileBasedConfigurationManager/Main.java
+++
@@ -1,7 +0,0 @@
-package lld1.designPattern.singletonDesignPattern.assignments.fileBasedConfigurationManager;
-
-public class Main {
-    public static void main(String[] args) {
-        FileBasedConfigurationManagerImpl instance = FileBasedConfigurationManagerImpl.getInstance();
-    }
-}
